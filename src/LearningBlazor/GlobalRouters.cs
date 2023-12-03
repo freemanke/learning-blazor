08541cfa2123b5526b4e2d6b7f8dc78aeaf1b2a4
Freeman Ke <freemanke@qq.com>
2023-12-03 16:25:27 +0800

    dev

---
+++ src/LearningBlazor/GlobalRouters.cs
@@ -0,0 +1,9 @@
+namespace LearningBlazor;
+
+public static class GlobalRouters
+{
+    public const string Home = "/";
+    public const string Users = "users";
+    public const string Counter = "counter";
+    public const string Weather = "weather";
+}
